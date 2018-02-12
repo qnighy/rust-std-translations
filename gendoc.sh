8455e624705c94489a55b26c7e638e343c1228db
Masaki Hara <ackie.h.gmai@gmail.com>
2018-02-12 21:22:50 +0900

    Initial commit.

---
+++ gendoc.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -ue
+
+for crate in core alloc std_unicode std proc_macro; do
+  echo "rustdoc ja-JP/lib$crate/lib.rs..." >&2
+  rustdoc +nightly -o doc --crate-name $crate ja-JP/lib$crate/lib.rs
+done
+echo "done." >&2
