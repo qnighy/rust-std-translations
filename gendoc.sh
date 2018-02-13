a8e59171b44d7a8236443bdd6e1f6437c4e77017
Masaki Hara <ackie.h.gmai@gmail.com>
2018-02-13 15:16:58 +0900

    Remove proc_macro from documentation.

--- gendoc.sh
+++ gendoc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ue
 
-for crate in core alloc std_unicode std proc_macro; do
+for crate in core alloc std_unicode std; do
   echo "rustdoc ja-JP/lib$crate/lib.rs..." >&2
   rustdoc -o doc/ja-JP --crate-name $crate ja-JP/lib$crate/lib.rs
 done
